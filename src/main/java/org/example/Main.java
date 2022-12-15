34b3ae35d45c8c1ac452e7013e84dc86e52353a9
Katerina Latysheva <katenych@MBP-Katerina.attlocal.net>
2022-12-15 00:09:19 -0800

    added space

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         int n = 0, i = 0;
         while (n < 100) {
             if (!String.valueOf(i).contains("4") && !String.valueOf(i).contains("9")) {
