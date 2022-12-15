d6624645cc8977242e8130d2f72539411f5086f0
Katerina Latysheva <katenych@MBP-Katerina.attlocal.net>
2022-12-15 00:17:39 -0800

    description addad

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+// пронумерувати 100 космічних шатлів
         int n = 0, i = 0;
         while (n < 100) {
             if (!String.valueOf(i).contains("4") && !String.valueOf(i).contains("9")) {
